8af62f52565cb5e8c50f6a645224a7f1780be031
heynny-hurtado <halexandrahurtado@estudiante.uniajc.edu.co>
2026-09-13 21:44:51 +0000

    creamos el int para parres e impares

--- Taller_Uno/Ejercicio7.java
+++ Taller_Uno/Ejercicio7.java
@@ -1,5 +1,21 @@
-public class Ejercicio7{
-    //vamos a crear el arreglo de A de forma aleatoria
-    int []a = new int [10];
+public  class Ejercicio7{
+    
+
+    public static void main (String[]args){
+         //vamos a crear el arreglo de A
+    int[] a={1,2,3,4,5,6,7,8,9,10};
+
+    
+    for (int i = 0; i < a.length; i++) {
+            System.out.println ("a["+i+"]"+a[i]);
+    }
+
+    int np=0;
+    int ni= 0;
+
+    //creamos el arreghglo de los pares e impares
+
+    }
+}
 
 }
