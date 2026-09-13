f10bb5c7f4e725286433acdedbe29180ffcf4e85
heynny-hurtado <halexandrahurtado@estudiante.uniajc.edu.co>
2026-09-13 22:37:24 +0000

    ejercicio 7  perdón por tanto comentario

--- Taller_Uno/Ejercicio7.java
+++ Taller_Uno/Ejercicio7.java
@@ -5,10 +5,11 @@ public  class Ejercicio7{
          //vamos a crear el arreglo de A
     int[] a={1,2,3,4,5,6,7,8,9,10};
 
-    
+    //for para el arreglo a
     for (int i = 0; i < a.length; i++) {
             System.out.println ("a["+i+"]"+a[i]);
     }
+
     int[]pares =new int[a.length];  //para los pares
     int [] impares = new int[a.length]; //para los impares
 
@@ -16,17 +17,32 @@ public  class Ejercicio7{
     int ni= 0;
 
     //queremos hacer el for para los pares pero debemos agregar la condicion
-     for (int i = 0; i < 10; i++) {
-         if(a[i]%2 ==0){  //para dividir un numero en  2 partes y que no sobre un valor
-            //como si escogieramos un numero y vieramos cuantas veces se puede dividir ese numero
+    for (int i = 0; i < 10; i++) {
+         //como si escogieramos un numero y vieramos cuantas veces se puede dividir ese numero
+         if(a[i]%2 ==0){  //para dividir un numero en  2 partes y que no sobre un valor (si el numero es par)
             pares[np]=a[i];
             np = np+1;
-         }
-     }
+        }else{ // en el caso de que el numero  le falte algo por dividir o no se divida es impar
 
+            impares[ni]=a[i];
+            ni= ni+1;
+        }
+
+    
+    }//for para imprimir los pares
+    for (int i=0;i<np; i++ ){
+        System.out.println("pares["+i+"]="+pares[i]);
+    }
+    // for para imprimir los impares
+    for (int i = 0; i < ni; i++){
+        System.out.println("impares["+i+"]="+impares[i]);
+    }
+    
     }
     
     
     }
-}
+
+
+
 
