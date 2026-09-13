0ceba53b8a6523535f55eda68cf5d2e152008660
heynny-hurtado <halexandrahurtado@estudiante.uniajc.edu.co>
2026-09-13 22:19:11 +0000

    arreglo para los apres

--- Taller_Uno/Ejercicio7.java
+++ Taller_Uno/Ejercicio7.java
@@ -9,13 +9,24 @@ public  class Ejercicio7{
     for (int i = 0; i < a.length; i++) {
             System.out.println ("a["+i+"]"+a[i]);
     }
+    int[]pares =new int[a.length];  //para los pares
+    int [] impares = new int[a.length]; //para los impares
 
     int np=0;
     int ni= 0;
 
-    //creamos el arreghglo de los pares e impares
+    //queremos hacer el for para los pares pero debemos agregar la condicion
+     for (int i = 0; i < 10; i++) {
+         if(a[i]%2 ==0){  //para dividir un numero en  2 partes y que no sobre un valor
+            //como si escogieramos un numero y vieramos cuantas veces se puede dividir ese numero
+            pares[np]=a[i];
+            np = np+1;
+         }
+     }
 
+    }
+    
+    
     }
 }
 
-}
