913d26e7fe609e58f5b1f5d6fed0ca78eb5a2194
heynny-hurtado <halexandrahurtado@estudiante.uniajc.edu.co>
2026-09-14 23:52:45 +0000

    inicio del ejercicio matriz

---
+++ Taller_Uno/Matrices/Ejercicio17.java
@@ -0,0 +1,23 @@
+package Matrices;
+
+public class Ejercicio17 {
+    
+    public static void main(String[]args){
+
+    
+    //vamos a crear la matriz 
+        int [][]m={
+            {8,5,4},
+            {3,2,1},
+            {7,6,9},
+        };
+        //recorrido de la matriz
+        for(int i=0;i<m.length;i++){
+            for(int j=0;j<m[i].length;j++){
+
+            }
+            
+        }
+    }
+
+}
