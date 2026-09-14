a2527e1a3e341bd01d47acf86d552a591f0cb26d
heynny-hurtado <halexandrahurtado@estudiante.uniajc.edu.co>
2026-09-14 04:55:03 +0000

    inicio del ejercicio 10

--- Taller_Uno/Ejercicio10.java
+++ Taller_Uno/Ejercicio10.java
@@ -1,3 +1,18 @@
 public class Ejercicio10 {
-    
+    public static void main (String[]args){
+
+      //mision, sumar primero con ultimo segundo con penultimo etc cmomo lo vamos a hacer?
+        //quiero hacerlo creando  un arreglo que me de numeros aleatorios
+        int [] s = new int [12]; //mi arreglo lo llame s
+        int max= 15;
+        int min= 1;
+
+        //ahora si crearemos el for 
+
+        for (int i = 0; i<s.length; i++){
+            s[i]=(int)(Math.random()*(max-min+1))+min;
+            System.out.println("s["+i+"]="+s[i]); //Sysout para imprimir y visualizar el arreglo
+        }
+        
+    }
 }
