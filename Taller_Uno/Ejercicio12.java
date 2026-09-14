3b131ea2f34106f7577e5a4fe5c078fe7c25afd5
heynny-hurtado <halexandrahurtado@estudiante.uniajc.edu.co>
2026-09-14 21:06:54 +0000

    correciones y creaciones de los for

--- Taller_Uno/Ejercicio12.java
+++ Taller_Uno/Ejercicio12.java
@@ -12,15 +12,30 @@ public class Ejercicio12 {
         int x = 2;
         int p = 0; //p para posición
 
-        //creal el for para el arreglo a
-        for(int i = 0;i<a.length; i++){
+        //tambien usar el random para que me de los numeros
+        int max= 6;
+        int min = 1;
 
-            // el if para X
-            if (a[i]==x){ b[p]=i;p++;
+        //creal el for para buscar en el arreglo a
+        for(int i = 0;i<a.length; i++){
+            a[i]= (int)(Math.random()* (max-min+1))+min;
+            System.out.println("a["+i+"]="+a[i]);
                 
             }
+            //for qye busca el numero x  y la condicion que guarda las posiciones en b
+            for(int i = 0;i<a.length; i++){
+             // el if para X
+            if (a[i]==x){ b[p]=i;p++;
 
-            
+            }
+        }
+
+        //ahora si el for que recorre todo el arreglo a
+        for (int i = 0; i <a.length;i++){
+            // la condicion que busca si en la posición i se encuentra el numero X que buscamos
+            if(a[i]==x);{
+                b[p]= i; p++;
+            }
         }
     }
     
