30beb284934501d2d50326d70ea62a68a5347588
heynny-hurtado <halexandrahurtado@estudiante.uniajc.edu.co>
2026-09-14 20:27:31 +0000

    creación de arreglos creacion de x

--- Taller_Uno/Ejercicio12.java
+++ Taller_Uno/Ejercicio12.java
@@ -1,6 +1,17 @@
 public class Ejercicio12 {
     public static void main (String[]args ){
         
+        
+        //Crear el arreglo a
+        int[] a =new int[12];
+
+        //creamos el arreglo donde vamos a ver las posiciones
+        int[]b= new int[a.length];
+
+        //crear x
+        int x = 2;
+        int p = 0; //p para posición
+        
     }
     
 }
