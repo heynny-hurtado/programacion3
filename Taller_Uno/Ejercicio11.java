93abcc9fd2edbae70c1cf5bfc9d94e677bab199e
heynny-hurtado <halexandrahurtado@estudiante.uniajc.edu.co>
2026-09-14 05:28:48 +0000

    creacion del ejercicio 11 y comentario de lo que queremos hacer

---
+++ Taller_Uno/Ejercicio11.java
@@ -0,0 +1,8 @@
+public class Ejercicio11 {
+
+    public static void main (String[]agrs){
+        //debemos de un arreglo con n numeros generar 3 arreglos uno + otro - otro con 0
+        
+    }
+    
+}
