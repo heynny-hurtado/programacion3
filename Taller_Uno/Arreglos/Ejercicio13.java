05d6615ab727e111a18316a58343f65221b6b691
heynny-hurtado <halexandrahurtado@estudiante.uniajc.edu.co>
2026-09-14 22:46:45 +0000

    creación de los arreglos

--- Taller_Uno/Arreglos/Ejercicio13.java
+++ Taller_Uno/Arreglos/Ejercicio13.java
@@ -1,4 +1,15 @@
 package Arreglos;
 public class Ejercicio13 {
+
+    //crear el arreglo de a
+    int []a =new int[a.length];
+
+    //crreamos el arreglo de los mayores y menores
+    int[ ]mayores = new int[a.length];
+    int[]menores = new int [a.length];
+    
+    //Rango de los aleatorios 
+    int max = 20;
+    int min: 1;
     
 }
