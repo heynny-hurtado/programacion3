9bd2035e8db4a2505a32ffb8f073f6ac7d8a13fd
heynny-hurtado <halexandrahurtado@estudiante.uniajc.edu.co>
2026-09-15 02:19:51 +0000

    creación de la matriz y los for

--- Taller_Uno/Matrices/Ejercicio20.java
+++ Taller_Uno/Matrices/Ejercicio20.java
@@ -4,6 +4,13 @@ public class Ejercicio20 {
     public static void main(String[]args){
         //haremos practicamente lo mismo que el anterior solo que cambia un poco
 
-        
+        int[][] m = new int[10][10];
+
+        for(int i =0; i<m.length; i++){
+
+            for(int j = 0; j<m[i].length;j++){
+
+            }
+        }
     }
 }
