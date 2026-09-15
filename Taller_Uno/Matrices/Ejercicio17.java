8116c291bcac805f3b24cb10dfc6b2ddcbb5824b
heynny-hurtado <halexandrahurtado@estudiante.uniajc.edu.co>
2026-09-15 00:28:55 +0000

    arreglos de errores al probar la matriz

--- Taller_Uno/Matrices/Ejercicio17.java
+++ Taller_Uno/Matrices/Ejercicio17.java
@@ -3,7 +3,8 @@ package Matrices;
 public class Ejercicio17 {
     
     public static void main(String[]args){
-
+        //i representa las fila, horizontal
+        //j las columnas vertical
     
     //vamos a crear la matriz  m
         int [][]m={
@@ -14,20 +15,22 @@ public class Ejercicio17 {
         //recorrido de la matriz m 
         for(int i=0;i<m.length;i++){
             for(int j=0;j<m[i].length;j++){
+
                  
             System.out.println("m["+i+"]["+j+"]="+m[i][j]);
             }
            
         }
         //for para la suma de filas 
-        int sumaF=0;
+     
         
         for(int i = 0; i< m.length;i++){
+               int sumaF=0;
 
             for(int j=0; j<m[i].length; j++){
                 sumaF = sumaF+m[i][j];
             }
-            System.out.println("La suma de la dula es"+i+"="+ sumaF);
+            System.out.println("La suma de la fila es"+i+"="+ sumaF);
         }
     }
 
