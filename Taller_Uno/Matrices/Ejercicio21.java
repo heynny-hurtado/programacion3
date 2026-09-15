ee000cd5b6738696f72ee8414970eead4c9387a8
heynny-hurtado <halexandrahurtado@estudiante.uniajc.edu.co>
2026-09-15 02:48:13 +0000

    primera parte for, crear los random, impresion

--- Taller_Uno/Matrices/Ejercicio21.java
+++ Taller_Uno/Matrices/Ejercicio21.java
@@ -10,8 +10,21 @@ public class Ejercicio21 {
         int[]b = new int[a.length];
 
         //vamos a hacer uno rango  de los numeros random  para que los numero se repitan
-        int max = 9;
+        int max = 8;
         int min = 1;
+
+
+        //realizamos los for
+        for (int i = 0; i< a.length; i++){
+            for (int j =0; j<a[i].length;j++){
+            
+                //creamos el random
+                a[i][j]=(int)(Math.random()*(max-min+1))+min;
+                //imprimimos
+                System.out.print(a[i][j]+" ");
+            }
+            System.out.println();
+        }
 
     }
 }
