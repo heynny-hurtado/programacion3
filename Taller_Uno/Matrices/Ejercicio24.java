be0fbdb40611261f2b3026c27be0e2c802ee8753
heynny-hurtado <halexandrahurtado@estudiante.uniajc.edu.co>
2026-09-15 04:24:22 +0000

    inicio

---
+++ Taller_Uno/Matrices/Ejercicio24.java
@@ -0,0 +1,22 @@
+package Matrices;
+
+public class Ejercicio24 {
+
+    public static void main(String[]args){
+
+        //meses  
+        String[] meses = {
+            "Enero","Febrero", "Marzo","Abril",
+            "Mayo","Junio", "Julio", "Agosto",
+            "Septiembrw","Octubre", "Noviembre", "Diciembre",
+        };
+
+        String[] cereales = {
+            "Arroz","Granola", "sucaritas", "trigo",
+        };
+
+        //los mese por los cereales
+
+    }
+    
+}
