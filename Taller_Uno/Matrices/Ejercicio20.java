b65389bc84bb34f67273c2b01435dbdd0f3748e6
heynny-hurtado <halexandrahurtado@estudiante.uniajc.edu.co>
2026-09-15 02:13:33 +0000

    creacion del archivo ejercicio20

---
+++ Taller_Uno/Matrices/Ejercicio20.java
@@ -0,0 +1,9 @@
+package Matrices;
+
+public class Ejercicio20 {
+    public static void main(String[]args){
+        //haremos practicamente lo mismo que el anterior solo que cambia un poco
+
+        
+    }
+}
