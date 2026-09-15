d8395c2ceb1477ef319cbd41fa05e9c756d663d5
heynny-hurtado <halexandrahurtado@estudiante.uniajc.edu.co>
2026-09-15 00:53:08 +0000

    creacion del archivo ejercicio 18

---
+++ Taller_Uno/Matrices/Ejercicio18.java
@@ -0,0 +1,11 @@
+package Matrices;
+
+public class Ejercicio18 {
+    public static void main(String[]args){
+
+        //ahora queremos hacer una matriz que haga la tabla del1 al 10
+        //es como si fueramos a multiplicar la fila por la columna 
+        
+    }
+    
+}
