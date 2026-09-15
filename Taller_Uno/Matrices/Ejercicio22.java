06bc716c03ebb27cf10dea2c7e0e54a14d5d2b7b
heynny-hurtado <halexandrahurtado@estudiante.uniajc.edu.co>
2026-09-15 03:50:12 +0000

    creacion de los for  y las condiciones

--- Taller_Uno/Matrices/Ejercicio22.java
+++ Taller_Uno/Matrices/Ejercicio22.java
@@ -37,7 +37,26 @@ public class Ejercicio22 {
             int cMayor = 0;
             int fMenor = 0;
             int cMenor = 0;
-            
+
+            //buscamos el mayor y buscamos el menor con los for y los if para las condiciones
+            for (int i = 0 ; i<a.length; i ++){
+                for (int j = 0;j<a[i].length; j++){
+                    if (a[i][j]> mayor){
+                        fMayor = i;
+                        cMayor = j;
+                    }
+                    if(a[i][j]<menor){
+                        menor= a[i][j];
+                        fMayor = i;
+                        cMenor = j;
+                    }
+
+                }
+            }
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
 
         }
 
