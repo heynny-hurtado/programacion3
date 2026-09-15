0f39175143e627f65ca266920e55a78df4df721e
heynny-hurtado <halexandrahurtado@estudiante.uniajc.edu.co>
2026-09-15 01:51:25 +0000

    creación de los for

---
+++ Taller_Uno/Matrices/Ejercicio19.java
@@ -0,0 +1,18 @@
+package Matrices;
+
+public class Ejercicio19 {
+    public static void main(String[]args){
+
+      //creamos la matris 
+      int [][]m = new int [6][6]; 
+
+      //vamso a crear los for
+      for (int i = 0; i <m.length; i++){
+
+        for(int j = 0; j<m[i].length;j++){
+            
+        }
+      }
+    }
+    
+}
