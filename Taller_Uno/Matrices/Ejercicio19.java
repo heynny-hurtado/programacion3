f4bdd51a476bf3a119de9d1a77ae67214917a437
heynny-hurtado <halexandrahurtado@estudiante.uniajc.edu.co>
2026-09-15 02:10:42 +0000

    condiciones e impresiones del programa

--- Taller_Uno/Matrices/Ejercicio19.java
+++ Taller_Uno/Matrices/Ejercicio19.java
@@ -6,12 +6,23 @@ public class Ejercicio19 {
       //creamos la matris 
       int [][]m = new int [6][6]; 
 
-      //vamso a crear los for
+      //vamso a crear los for para recorrer las filas y el de recorrer las columnas
       for (int i = 0; i <m.length; i++){
 
         for(int j = 0; j<m[i].length;j++){
-            
+
+            //crearemos las condiciones que queremos
+            if(i==j){
+                m[i][j]=1;
+
+            }else{
+                m[i][j]=0;
+            }
+            //Imprimimos
+            System.out.print(m[i][j]+"");
+
         }
+        System.out.println(); //para que ahora si lo imprima todo
       }
     }
     
