e8623dd31d31a5ca2b973d404e57cf131817c793
heynny-hurtado <halexandrahurtado@estudiante.uniajc.edu.co>
2026-09-15 00:15:46 +0000

    finalizacion y prueba de la matriz

--- Taller_Uno/Matrices/Ejercicio17.java
+++ Taller_Uno/Matrices/Ejercicio17.java
@@ -5,19 +5,29 @@ public class Ejercicio17 {
     public static void main(String[]args){
 
     
-    //vamos a crear la matriz 
+    //vamos a crear la matriz  m
         int [][]m={
             {8,5,4},
             {3,2,1},
             {7,6,9},
         };
-        //recorrido de la matriz
+        //recorrido de la matriz m 
         for(int i=0;i<m.length;i++){
             for(int j=0;j<m[i].length;j++){
-
-            }
-            //suma de las filas 
+                 
             System.out.println("m["+i+"]["+j+"]="+m[i][j]);
+            }
+           
+        }
+        //for para la suma de filas 
+        int sumaF=0;
+        
+        for(int i = 0; i< m.length;i++){
+
+            for(int j=0; j<m[i].length; j++){
+                sumaF = sumaF+m[i][j];
+            }
+            System.out.println("La suma de la dula es"+i+"="+ sumaF);
         }
     }
 
