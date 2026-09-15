e922dd1de9698085e75a4b8446ffaa86ce1e3ce7
heynny-hurtado <halexandrahurtado@estudiante.uniajc.edu.co>
2026-09-15 03:38:21 +0000

    continuacion

--- Taller_Uno/Matrices/Ejercicio22.java
+++ Taller_Uno/Matrices/Ejercicio22.java
@@ -1,9 +1,35 @@
 package Matrices;
 
+import java.io.StringReader;
+
 public class Ejercicio22 {
     
     public static void main (String[]args){
 
-        
+        int [][]a = new int [f][c];
+
+
+
+        // creamos el for para llenar
+        for(int i =0; i<a.length; i++){
+            for (int j = 0; j <a[i].length;j++){
+                a[i][j]= (int)(Math.random()* 100+1);
+    
+            }
+        }
+
+        //mostrar la matriz
+        String  cad ="";
+
+        for(int i = 0; i<a.length;i++){
+            for(int j = 0; j <a[i].length;j++){
+                cad+= "|"+a[i][j]+"|";
+            }
+            cad+= "n";
+        }
+
+
+
+
     }
 }
