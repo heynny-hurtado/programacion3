49e5ac9f6cc88c308f2b854efb418ac6fab863e0
heynny-hurtado <halexandrahurtado@estudiante.uniajc.edu.co>
2026-09-15 01:25:34 +0000

    varios arreglos

--- Taller_Uno/Matrices/Ejercicio18.java
+++ Taller_Uno/Matrices/Ejercicio18.java
@@ -10,6 +10,25 @@ public class Ejercicio18 {
 
         //vamos a crear la matriz 
         int[][]m = new int[10][10]; //con esto sabremo que seran 10filas y 10 columnas
+        
+        //creamos el for
+        for(int i = 0; i<m.length;i++){
+
+            for(int j =0;j < m[i].length; j++){
+                m[i][j]= (i+1)*(j+1);
+            }
+        }
+        //ahora si imprimimos
+        System.err.println("TABLA DE MULTIPLICAR");
+
+        for (int i =0; i <m.length; i++){
+
+            for(int j = 0; j<m[i].length; j++){
+                //para que no salte a la siguiente linea sino que siga en la misma, toco investigar profe
+                System.out.print(m[i][j]+"\t" );
+            }
+        }
+
     }
     
 }
