3731628d1a5b609e736f7ebce8a981e7a07b8119
heynny-hurtado <halexandrahurtado@estudiante.uniajc.edu.co>
2026-09-15 02:42:10 +0000

    creación de la matriz

---
+++ Taller_Uno/Matrices/Ejercicio21.java
@@ -0,0 +1,17 @@
+package Matrices;
+
+public class Ejercicio21 {
+    public static void main(String[]args){
+
+
+        int[][]a= new int [3][4];
+
+        //vamos a crear el vector
+        int[]b = new int[a.length];
+
+        //vamos a hacer uno rango  de los numeros random  para que los numero se repitan
+        int max = 9;
+        int min = 1;
+
+    }
+}
