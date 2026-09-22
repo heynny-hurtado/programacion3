eaec9c8db99bedb304608b952613009eeff6848b
heynny-hurtado <halexandrahurtado@estudiante.uniajc.edu.co>
2026-09-22 17:45:40 +0000

    prototipado del menu principal

--- Parcial 1/MenuPrincipal.java
+++ Parcial 1/MenuPrincipal.java
@@ -5,11 +5,41 @@ public class MenuPrincipal{
     public static void main(String[]args){
 
         Scanner entrada = new Scanner(System.in);
-
         int opcion;
 
         do{
+            System.out.println("===========CINEMASTAR===============");
+            System.out.println("1. Crear peliculas"); 
+            System.out.println("2. asignar funcions"); 
+            System.out.println("3. Venter entradas"); 
+            System.out.println("4. salir");  
             
-        }
+            opcion= entrada.nextInt();
+
+
+            switch(opcion){
+
+                case 1:
+                System.out.println();
+                break;
+
+                case 2:
+                System.out.println();
+                break;
+
+                case 3:
+                SySystem.out.println();
+                break;
+
+                case 4:
+                    break;
+
+                    default:
+
+
+            }
+
+            
+        } while (opcion != 4);
     }
 }
